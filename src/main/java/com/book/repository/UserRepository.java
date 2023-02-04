364172cad4a7cc7617234b5331f6ab4cad54c34d
Nhat Anh <54491115+ZNhatAnhZ@users.noreply.github.com>
2023-02-04 08:08:33 +0700

    return jwt token and implement authorized routes (#5)

--- src/main/java/com/book/repository/UserRepository.java
+++ src/main/java/com/book/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.book.model.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UsersEntity, Integer> {
     Boolean existsByUserName(String username);
+    Optional<UsersEntity> findUsersEntityByUserName(String username);
 }
