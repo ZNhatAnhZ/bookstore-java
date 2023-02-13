0eed36f451f60556fd0bdef8d34f3b061fcb967c
Nhat Anh <54491115+ZNhatAnhZ@users.noreply.github.com>
2023-02-13 13:57:38 +0700

    completed API to get recommended products (#16)

--- src/main/java/com/book/controller/ProductsController.java
+++ src/main/java/com/book/controller/ProductsController.java
@@ -3,15 +3,15 @@ package com.book.controller;
 import com.book.dto.ProductsDTO;
 import com.book.model.ProductsEntity;
 import com.book.service.ProductsService;
-import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,6 +50,17 @@ public class ProductsController {
         Optional<Page<ProductsEntity>> result = productsService.findAllByProductNameContainingIgnoreCase(page, size, productName);
 
         return getProductsDTOResponseEntity(result);
+    }
+
+    @GetMapping("/getRecommendedProducts")
+    public ResponseEntity<List<ProductsEntity>> getRecommendedProducts(@RequestParam int userId) throws IOException {
+        Optional<List<ProductsEntity>> result = productsService.getRecommendedProducts(userId);
+
+        if (result.isPresent()) {
+            return new ResponseEntity<>(result.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity<ProductsDTO> getProductsDTOResponseEntity(Optional<Page<ProductsEntity>> result) {
